307251d753129b4db063a780328e0886e8ed7078
Maria108 <maria.abash@gmail.com>
2018-03-21 19:29:27 -0400

    Eight commit

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
 let gambMenu = document.getElementById("gamburger")
 
-gambMenu.addEventListener("click", (event) => {
-    document.getElementById("newMenu").style.display = "block";
-})
+gambMenu.addEventListener("click", toggleMenu)
+
+function toggleMenu(event) {
+    let newMenu = document.getElementById("newMenu")
+    if (newMenu.style.display === "block") {
+        newMenu.style.display = "none"
+    } else {
+        newMenu.style.display = "block"
+    }
+}
+
+// let navBar = document.getElementById('nav')
+
+// window.addEventListener('scroll', (event) => {
+//     if (window.scrollY === 200) {
+//         navBar.className = "nav";
+//     }
+// });
+
+let slider = document.getElementById('slider');
+
+setInterval(() => {
+    let pos = Math.floor(Math.random() * 3) * 100;
+    slider.style.transform = `translateX(-${pos}%)`;
+}, 1500);
